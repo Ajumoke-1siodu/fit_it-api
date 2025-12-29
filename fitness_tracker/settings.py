b8217a9483532bd574a541484bae6a366ba98b5e
Ajumoke-1siodu <georgiaisiodu@gmail.com>
2025-12-29 01:44:06 -0800

    add changes

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -92,7 +92,8 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
     ],
     
-    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "DEFAULT_PAGINATION_CLASS":
+    "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 
     "EXCEPTION_HANDLER": "core.exceptions.custom_exception_handler",
