496dd50402b55656f681e14a5f07686f522dda34
Ajumoke-1siodu <georgiaisiodu@yahoo.com>
2025-12-29 13:34:22 +0000

    Fix settings.py syntax and add requirements.txt add changes

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -81,6 +81,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "EXCEPTION_HANDLER": "core.exceptions.custom_exception_handler",
+
 }
 
 
