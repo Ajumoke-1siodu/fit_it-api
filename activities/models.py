062a6b0260e658d45eb545746676e3d751376f73
Ajumoke-1siodu <georgiaisiodu@yahoo.com>
2025-12-31 11:37:41 +0000

    Fix syntax errors in Activity model

--- activities/models.py
+++ activities/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Activity(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    activity_type = models.CharField(max_length=50) 
-    duration = models.IntegerField()                                                           distance = models.FloatField(null=True, blank=True)
+    activity_type = models.CharField(max_length=50)
+    duration = models.IntegerField()
     calories_burned = models.IntegerField(null=True, blank=True)
-    date = models.DateField()                         
-    created_at = models.DateTimeField(auto_now_add=True
+    date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.activity_type} by {self.user.username} on {self.date}"
+
 
 # Create your models here.
